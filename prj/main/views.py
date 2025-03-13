53b38de773ba69aa7b3429ffe13a10a59e03c06d
Vojtěch Macek <vojtech.macek.s@gyarab.cz>
2025-03-13 21:49:58 +0100

    add files

--- prj/main/views.py
+++ prj/main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def homepage(request):
+    return render(request, 'main/homepage.html')
+
+def one(request):
+    return render(request, 'main/one.html')
+
+def two(request):
+    return render(request, 'main/two.html')
