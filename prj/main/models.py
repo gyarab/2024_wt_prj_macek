753a41e20ee9a76fa0f78d325ed4b2e64086ed11
Vojtěch Macek <vojtech.macek.s@gyarab.cz>
2025-03-14 10:21:29 +0100

    14.3. hodina

--- prj/main/models.py
+++ prj/main/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Food(models.Model):
+    name = models.CharField(max_length=300)
+    katgorie = models.IntegerField(blank=True, null=True)
