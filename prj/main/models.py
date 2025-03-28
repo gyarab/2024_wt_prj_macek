93db55e4e38852cc7ce73fb80e36cddc136c2d94
Vojtěch Macek <vojtech.macek.s@gyarab.cz>
2025-03-28 10:03:07 +0100

    idk

--- prj/main/models.py
+++ prj/main/models.py
@@ -3,3 +3,11 @@ from django.db import models
 class Food(models.Model):
     name = models.CharField(max_length=300)
     katgorie = models.IntegerField(blank=True, null=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
--- prj/prj/urls.py
+++ prj/prj/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from main import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='main/homepage.html'), name='homepage'), 
-    path('one/', TemplateView.as_view(template_name='main/one.html'), name='one'),
-    path('two/', TemplateView.as_view(template_name='main/two.html'), name='two'),
+    path('', TemplateView.as_view(template_name='main/homepage.html'), name='homepage'),
+    path('products/', views.ProductList.as_view(), name='product_list'),
+    path('product/<int:pk>/', views.ProductDetail.as_view(), name='product_detail'),
 ]
