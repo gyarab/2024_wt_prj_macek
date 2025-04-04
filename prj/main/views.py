d957692a50c771ae204977643fb9c2f5062e7db8
Vojtěch Macek <vojtech.macek.s@gyarab.cz>
2025-04-04 11:41:34 +0200

    hodina 4.4.

--- prj/main/views.py
+++ prj/main/views.py
@@ -1,16 +1,35 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Product
+from .models import Product, Category
 
 def homepage(request):
     return render(request, 'main/homepage.html')
 
 class ProductList(ListView):
     model = Product
-    template_name = 'main/ProductList.html'  # Opraveno na správnou cestu
+    template_name = 'main/ProductList.html'
     context_object_name = 'products'
+
+    def get_queryset(self):
+        """
+        Filtrování produktů podle kategorie.
+        """
+        category_id = self.kwargs.get('category_id')
+        if category_id:
+            return Product.objects.filter(category__id=category_id)
+        return Product.objects.all()
+
+    def get_context_data(self, **kwargs):
+        """
+        Přidání kategorie do kontextu pro šablonu.
+        """
+        context = super().get_context_data(**kwargs)
+        category_id = self.kwargs.get('category_id')
+        if category_id:
+            context['category'] = Category.objects.get(id=category_id)
+        return context
 
 class ProductDetail(DetailView):
     model = Product
-    template_name = 'main/ProductDetail.html'  # Opraveno na správnou cestu
+    template_name = 'main/ProductDetail.html'
     context_object_name = 'product'
--- prj/prj/urls.py
+++ prj/prj/urls.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from main import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='main/homepage.html'), name='homepage'),
-    path('products/', views.ProductList.as_view(), name='product_list'),
+    path('products/', views.ProductList.as_view(), name='product_list'),  # Zobrazení všech produktů
+    path('category/<int:category_id>/', views.ProductList.as_view(), name='product_list_by_category'),  # Zobrazení produktů podle kategorie
     path('product/<int:pk>/', views.ProductDetail.as_view(), name='product_detail'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
