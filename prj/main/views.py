a12cf922ac4bf222541c8b15e7e635c9eea87aff
Vojtěch Macek <vojtech.macek.s@gyarab.cz>
2025-04-12 22:35:22 +0200

    12.4. changes

--- prj/main/views.py
+++ prj/main/views.py
@@ -1,16 +1,39 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Product
-
-def homepage(request):
-    return render(request, 'main/homepage.html')
+from .models import Product, Category
 
 class ProductList(ListView):
     model = Product
-    template_name = 'main/ProductList.html'  # Opraveno na správnou cestu
+    template_name = 'main/ProductList.html'
     context_object_name = 'products'
+
+    def get_queryset(self):
+        category_id = self.request.GET.get('category')
+        subcategory_id = self.request.GET.get('subcategory')
+        
+        # Pokud je podkategorie, filtruj produkty podle ní
+        if subcategory_id:
+            return Product.objects.filter(category_id=subcategory_id)
+        
+        # Pokud je hlavní kategorie, filtruj podle ní
+        if category_id:
+            return Product.objects.filter(category__parent_id=category_id)
+        
+        return Product.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['main_categories'] = Category.objects.filter(parent__isnull=True)  # Hlavní kategorie
+        context['selected_category'] = self.request.GET.get('category')
+        context['selected_subcategory'] = self.request.GET.get('subcategory')
+        
+        # Pro každou hlavní kategorii načteme její podkategorie
+        if context['selected_category']:
+            context['subcategories'] = Category.objects.filter(parent_id=context['selected_category'])
+        
+        return context
 
 class ProductDetail(DetailView):
     model = Product
-    template_name = 'main/ProductDetail.html'  # Opraveno na správnou cestu
+    template_name = 'main/ProductDetail.html'
     context_object_name = 'product'
--- prj/prj/urls.py
+++ prj/prj/urls.py
@@ -2,10 +2,16 @@ from django.contrib import admin
 from django.urls import path
 from main import views
 from django.views.generic import TemplateView
+from django.conf import settings 
+from django.conf.urls.static import static 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='main/homepage.html'), name='homepage'),
     path('products/', views.ProductList.as_view(), name='product_list'),
+    path('products/category/<int:category_id>/', views.ProductList.as_view(), name='category_products'),
     path('product/<int:pk>/', views.ProductDetail.as_view(), name='product_detail'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
