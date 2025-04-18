dbf1b405d897f19a023552dc69b65b41f917c416
Vojtěch Macek <vojtech.macek.s@gyarab.cz>
2025-04-18 16:41:53 +0200

    add cart.html

--- prj/main/views.py
+++ prj/main/views.py
@@ -1,12 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 from .models import Product, Category
-<<<<<<< HEAD
-=======
-
-def homepage(request):
-    return render(request, 'main/homepage.html')
->>>>>>> d957692a50c771ae204977643fb9c2f5062e7db8
 
 class ProductList(ListView):
     model = Product
@@ -14,51 +8,32 @@ class ProductList(ListView):
     context_object_name = 'products'
 
     def get_queryset(self):
-<<<<<<< HEAD
         category_id = self.request.GET.get('category')
         subcategory_id = self.request.GET.get('subcategory')
-        
-        # Pokud je podkategorie, filtruj produkty podle ní
+
         if subcategory_id:
             return Product.objects.filter(category_id=subcategory_id)
-        
-        # Pokud je hlavní kategorie, filtruj podle ní
+
         if category_id:
             return Product.objects.filter(category__parent_id=category_id)
-        
+
         return Product.objects.all()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['main_categories'] = Category.objects.filter(parent__isnull=True)  # Hlavní kategorie
+        context['main_categories'] = Category.objects.filter(parent__isnull=True)
         context['selected_category'] = self.request.GET.get('category')
         context['selected_subcategory'] = self.request.GET.get('subcategory')
-        
-        # Pro každou hlavní kategorii načteme její podkategorie
+
         if context['selected_category']:
             context['subcategories'] = Category.objects.filter(parent_id=context['selected_category'])
-        
-=======
-        """
-        Filtrování produktů podle kategorie.
-        """
-        category_id = self.kwargs.get('category_id')
-        if category_id:
-            return Product.objects.filter(category__id=category_id)
-        return Product.objects.all()
 
-    def get_context_data(self, **kwargs):
-        """
-        Přidání kategorie do kontextu pro šablonu.
-        """
-        context = super().get_context_data(**kwargs)
-        category_id = self.kwargs.get('category_id')
-        if category_id:
-            context['category'] = Category.objects.get(id=category_id)
->>>>>>> d957692a50c771ae204977643fb9c2f5062e7db8
         return context
 
 class ProductDetail(DetailView):
     model = Product
     template_name = 'main/ProductDetail.html'
     context_object_name = 'product'
+
+class CartView(TemplateView):
+    template_name = 'main/Cart.html'
--- prj/prj/urls.py
+++ prj/prj/urls.py
@@ -4,26 +4,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 from main import views
 from django.views.generic import TemplateView
-from django.conf import settings 
-from django.conf.urls.static import static 
+from main.views import ProductList, ProductDetail, CartView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='main/homepage.html'), name='homepage'),
-<<<<<<< HEAD
     path('products/', views.ProductList.as_view(), name='product_list'),
     path('products/category/<int:category_id>/', views.ProductList.as_view(), name='category_products'),
-=======
-    path('products/', views.ProductList.as_view(), name='product_list'),  # Zobrazení všech produktů
-    path('category/<int:category_id>/', views.ProductList.as_view(), name='product_list_by_category'),  # Zobrazení produktů podle kategorie
->>>>>>> d957692a50c771ae204977643fb9c2f5062e7db8
     path('product/<int:pk>/', views.ProductDetail.as_view(), name='product_detail'),
+    path('cart/', CartView.as_view(), name='cart'),
 ]
 
 if settings.DEBUG:
-<<<<<<< HEAD
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-=======
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
->>>>>>> d957692a50c771ae204977643fb9c2f5062e7db8
